8f437df1c6b4fb861d1d3c1d0bcac7f9407d8df1
Liu Leisexport <greenborn101@gmail.com>
2021-05-26 21:39:12 +0200

    adjusted the info, landing page and loading page

--- src/screens/About.js
+++ src/screens/About.js
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <View style={styles.container}>
       <ScrollView showsVerticalScrollIndicator={false}>
-        <Text style={styles.header}>Kictstart the new data economy</Text>
+        <Text style={styles.header}>Kickstart the new data economy</Text>
         <Text style={styles.text}>
           Data is the new oil in the 21st century and big cooperations have
           already realised that to their advantage. In this project we are
@@ -14,25 +14,28 @@ const About = () => {
         </Text>
         <Text style={styles.text}>
           By using a reward system based on datatokens the contributors become
-          immediate shareholders in data unions. Our first Data Vault is for
+          immediate co-owners in data unions. Our first Data Vault is for
           images. It is available on the Ocean Protocol marketplace. The
-          contributors upload them, annotate them and then these contributors
+          contributors upload images, annotate them and then these contributons
           are verified. They are rewarded with datatokens so they become
-          shareholders in the dataset.
+          co-owners of the dataset.
         </Text>
         <Text style={styles.text}>
           This combination of mechanics creates an intrinsic motivation to
           contribute positively. We want to give everyone the ability to use
           their data for a better future and their own profit. The project is
-          powered by Ocean Protocol. They enable us with their grans from the
-          OceanDAO to work on the project but without their technology we could
+          powered by Ocean Protocol. They enable us, with their grants from the
+          OceanDAO, to work on the project but without their technology we could
           never realise our project.
         </Text>
         <Text style={styles.text}>
-          The main technologies advances that they created are datastrokes which
-          we use as our economy for our data. And compute-to-data which we use
+          The main technological advantages that they created are datastokens which
+          we use as the economy and price discovery for our data. And compute-to-data which we use
           to sell our collected dataset to buyers for algorithm training without
           the risk of our data being copied.
+        </Text>
+        <Text style={styles.text}>
+          For 
         </Text>
       </ScrollView>
     </View>
